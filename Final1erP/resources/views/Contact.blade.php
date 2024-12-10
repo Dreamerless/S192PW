f9fb93ef59f98ab85f463390d874b2642e3b582b
Maximiliano Acosta <122043750@upq.edu.mx>
2024-12-09 19:19:14 -0600

    FinalPrimerParcial
    Finalizado pirmer parcial

---
+++ Final1erP/resources/views/Contact.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Lista de Contactos </title>
+    @vite(['resources/js/app.js'])
+
+    
+
+</head>
+
+
+<body>
+
+    <h1 class="display-1 text-warning text-center"> Listado de Contactos </h1>
+
+    <a href="{{ url('/Form') }}"> Regresa al formulario </a>
+    <a href="{{ url('/') }}"> Regresa al inicio </a>
+
+     
+</body>
+
+</html>
